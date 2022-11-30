eb3398bc994637e45d0c7b10a0eec1f83cc9e1e2
OttaviaFeletig <ottafel@hotmail.it>
2022-11-30 11:32:26 +0100

    reading input and converting date

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ import { argv } from "node:process";
 import fs from "fs";
 import { parse } from "csv-parse";
 
-console.log(argv);
+const stringToDate = (date) => {
+  const splitDate = date.replace(";", "").split("/");
+  return new Date(
+    splitDate[2],
+    parseInt(splitDate[1]) - 1,
+    parseInt(splitDate[0]) + 1
+  );
+};
+
+const readInput = () => {
+  if (argv.length === 5) {
+    const path = argv[2];
+    const column = parseInt(argv[3]);
+    const key = column === 3 ? stringToDate(argv[4]) : argv[4];
+    console.log(path, column, key);
+  } else {
+    console.log("Incorrect input");
+  }
+};
+readInput();
